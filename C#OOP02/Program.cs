496713573014c399ad18882ac90d91cebe57d126
7oda <mlieegy@gmail.com>
2026-09-22 11:50:46 +0300

    Comment the code in main

--- C#OOP02/Program.cs
+++ C#OOP02/Program.cs
@@ -20,93 +20,95 @@
 
             #region Question06
 
-            //a. Create a DeliveryCenter.
-            DeliveryCenter deliveryCenter = new DeliveryCenter();
+            ////a. Create a DeliveryCenter.
+            //DeliveryCenter deliveryCenter = new DeliveryCenter();
 
-            //b. Read data for three shipments from the user.
-            for (int i = 0; i < 3; i++)
-            {
-                Console.WriteLine($"Enter shipment {i+1} Data");
+            ////b. Read data for three shipments from the user.
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine($"Enter shipment {i+1} Data");
 
-                Console.Write("Tracking code: ");
-                string trackingCode = Console.ReadLine();
+            //    Console.Write("Tracking code: ");
+            //    string trackingCode = Console.ReadLine();
 
-                Console.Write("Description: ");
-                string description = Console.ReadLine();
+            //    Console.Write("Description: ");
+            //    string description = Console.ReadLine();
 
-                double weight;
-                while (true)
-                {
-                    Console.Write("Weight: ");
-                    bool isParsed = double.TryParse(Console.ReadLine(), out weight);
-                    if (isParsed)
-                        break;
-                }
+            //    double weight;
+            //    while (true)
+            //    {
+            //        Console.Write("Weight: ");
+            //        bool isParsed = double.TryParse(Console.ReadLine(), out weight);
+            //        if (isParsed)
+            //            break;
+            //    }
 
-                double deliveryFee;
-                while (true)
-                {
-                    Console.Write("Delivery fee: ");
-                    bool isParsed = double.TryParse(Console.ReadLine(), out deliveryFee);
-                    if (isParsed)
-                        break;
-                }
+            //    double deliveryFee;
+            //    while (true)
+            //    {
+            //        Console.Write("Delivery fee: ");
+            //        bool isParsed = double.TryParse(Console.ReadLine(), out deliveryFee);
+            //        if (isParsed)
+            //            break;
+            //    }
 
-                Console.Write("City: ");
-                string city = Console.ReadLine();
+            //    Console.Write("City: ");
+            //    string city = Console.ReadLine();
 
-                Console.Write("Street: ");
-                string street = Console.ReadLine();
+            //    Console.Write("Street: ");
+            //    string street = Console.ReadLine();
 
-                int buildingNumber;
-                while (true)
-                {
-                    Console.Write("Building number: ");
-                    bool isParsed = int.TryParse(Console.ReadLine(), out buildingNumber);
-                    if (isParsed)
-                        break;
-                }
-                Console.WriteLine("----------------");
-                DeliveryAddress destination = new DeliveryAddress(city, street, buildingNumber);
+            //    int buildingNumber;
+            //    while (true)
+            //    {
+            //        Console.Write("Building number: ");
+            //        bool isParsed = int.TryParse(Console.ReadLine(), out buildingNumber);
+            //        if (isParsed)
+            //            break;
+            //    }
+            //    Console.WriteLine("----------------");
+            //    DeliveryAddress destination = new DeliveryAddress(city, street, buildingNumber);
 
-                //c. Create each Shipment and add it to the DeliveryCenter.
-                Shipment shipment = new Shipment(trackingCode, description, weight, deliveryFee, destination);
-                deliveryCenter.AddShipment(shipment);
-            }
-            Console.WriteLine("Shipments added successfully");
+            //    //c. Create each Shipment and add it to the DeliveryCenter.
+            //    Shipment shipment = new Shipment(trackingCode, description, weight, deliveryFee, destination);
+            //    deliveryCenter.AddShipment(shipment);
+            //}
+            //Console.WriteLine("Shipments added successfully");
 
-            //d. Print the three shipments using the integer indexer.
-            Console.WriteLine("===== All Shipments =====");
-            for(int i = 0; i < 3; i++)
-            {
-                Console.WriteLine($"Shipment {i+1}: ");
-                Shipment shipment = deliveryCenter[i];
-                shipment.PrintShipment();
-                Console.WriteLine("----------------");
-            }
+            ////d. Print the three shipments using the integer indexer.
+            //Console.WriteLine("===== All Shipments =====");
+            //for(int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine($"Shipment {i+1}: ");
+            //    Shipment shipment = deliveryCenter[i];
+            //    shipment.PrintShipment();
+            //    Console.WriteLine("----------------");
+            //}
 
-            //e. Ask the user to enter a tracking code.
-            Console.Write("\nEnter a tracking code to search: ");
-            string searchCode = Console.ReadLine();
+            ////e. Ask the user to enter a tracking code.
+            //Console.Write("\nEnter a tracking code to search: ");
+            //string searchCode = Console.ReadLine();
 
-            //f. Search for the shipment using the string indexer.
-            Shipment found = deliveryCenter[searchCode];
+            ////f. Search for the shipment using the string indexer.
+            //Shipment found = deliveryCenter[searchCode];
 
-            //g. Print the shipment if found; otherwise print:Shipment not found.
-            if (!string.IsNullOrWhiteSpace(found.TrackingCode));
-                found.PrintShipment();
-            Console.WriteLine("Shipment not found");
+            ////g. Print the shipment if found; otherwise print:Shipment not found.
+            //if (!string.IsNullOrWhiteSpace(found.TrackingCode));
+            //    found.PrintShipment();
+            //Console.WriteLine("Shipment not found");
 
-            //h. Demonstrate the DeliveryAddress struct copy behavior.
-            Console.WriteLine("--- Demonstrate the DeliveryAddress struct copy behavior ---");
-            DeliveryAddress deliveryAddress01 = new DeliveryAddress("Cairo", "Tahrir Street", 10);
-            DeliveryAddress deliveryAddress02 = deliveryAddress01;  // Copy by value
-            deliveryAddress02.city = "Alexandria";   // not change in deliveryAddress01
-            Console.WriteLine(deliveryAddress01.city);
-            Console.WriteLine(deliveryAddress02.city);
+            ////h. Demonstrate the DeliveryAddress struct copy behavior.
+            //Console.WriteLine("--- Demonstrate the DeliveryAddress struct copy behavior ---");
+            //DeliveryAddress deliveryAddress01 = new DeliveryAddress("Cairo", "Tahrir Street", 10);
+            //DeliveryAddress deliveryAddress02 = deliveryAddress01;  // Copy by value
+            //deliveryAddress02.city = "Alexandria";   // not change in deliveryAddress01
+            //Console.WriteLine(deliveryAddress01.city);
+            //Console.WriteLine(deliveryAddress02.city);
 
             #endregion
             #endregion
+
+
         }
     }
 }
